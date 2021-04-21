72c604ac379fcd108c64095e22cac2b8599c89a3
EmilyEdna <847432003@qq.com>
2021-04-21 18:41:35 +0800

    ADD node

---
+++ AllSDK.Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using LightNovel.SDK;
+using System;
+
+namespace AllSDK.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            class1.T();
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ LightNovel.SDK/Class1.cs
@@ -0,0 +1,29 @@
+﻿using Synctool.HttpFramework;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LightNovel.SDK
+{
+    public class Class1
+    {
+        //https://github.com/0x7FFFFF/wenku8downloader/blob/master/src/user.py
+        private const string Host = "https://www.wenku8.net/login.php";
+
+        public Class1()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+
+        public void T()
+        {
+            var json = @"{'username': kilydoll365, 'password': sion8550, 'usercookie': 315360000, 'action': 'login',
+                      'submit': '%26%23160%3B%B5%C7%26%23160%3B%26%23160%3B%C2%BC%26%23160%3B'}";
+            var bytes = HttpMultiClient.HttpMulti.AddNode(Host,json,RequestType.POST).Build().RunBytes().FirstOrDefault();
+            using StreamReader reader = new StreamReader(new MemoryStream(bytes), Encoding.GetEncoding("GBK"));
+            var xx = reader.ReadToEnd();
+            Console.WriteLine(xx);
+        }
+    }
+}
