5d8e36e1e8696191106031eaacf0ebeb0af6e06c
EmilyEdna <847432003@qq.com>
2021-04-22 18:27:17 +0800

    Cookie

--- LightNovel.SDK/Class1.cs
+++ LightNovel.SDK/Class1.cs
@@ -1,7 +1,11 @@
 ﻿using Synctool.HttpFramework;
+using Synctool.LinqFramework;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 
 namespace LightNovel.SDK
@@ -18,9 +22,20 @@ namespace LightNovel.SDK
 
         public void T()
         {
-            var json = @"{'username': kilydoll365, 'password': sion8550, 'usercookie': 315360000, 'action': 'login',
-                      'submit': '%26%23160%3B%B5%C7%26%23160%3B%26%23160%3B%C2%BC%26%23160%3B'}";
-            var bytes = HttpMultiClient.HttpMulti.AddNode(Host,json,RequestType.POST).Build().RunBytes().FirstOrDefault();
+            List<KeyValuePair<string, string>> pairs = new List<KeyValuePair<string, string>>();
+            pairs.Add(new KeyValuePair<string, string>("username", "kilydoll365"));
+            pairs.Add(new KeyValuePair<string, string>("password", "sion8550"));
+            pairs.Add(new KeyValuePair<string, string>("usercookie", "0"));
+            pairs.Add(new KeyValuePair<string, string>("action", "login"));
+            pairs.Add(new KeyValuePair<string, string>("submit", "%26%23160%3B%B5%C7%26%23160%3B%26%23160%3B%C2%BC%26%23160%3B"));
+
+
+
+            var bytes = HttpMultiClient.HttpMulti.InitCookieContainer().AddNode(Host, pairs, RequestType.POST)
+                .Build().RunBytes(cookie =>
+                {
+                    var cok = cookie.GetCookies(new Uri(Host)).Cast<Cookie>().ToList();
+                }).FirstOrDefault();
             using StreamReader reader = new StreamReader(new MemoryStream(bytes), Encoding.GetEncoding("GBK"));
             var xx = reader.ReadToEnd();
             Console.WriteLine(xx);
